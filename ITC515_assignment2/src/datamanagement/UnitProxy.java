10fa351a6abaee029da53f726cf838bb49add32a
ThevaSL <senathipathi2001@gmail.com>
2016-09-05 16:54:52 +1000

    UnitProxy.java - Code Review Report
    1. Following class does not have a general comment / block of comment about the class and its functionality / methods. This will give more time for other codes or code review to understand, what kind of process undertaken in this class / what type of logic is been implemented. Plus White Space need to use that improve readbility by setting off sections of code that are logically related. 
    	Wrong Pratice / Not recommend approach :-
    	(No Comments)
    	package datamanagement;
    	public class UnitProxy implements IUnit {
        	private String UC;
        	private String un;
    	Best Practice / Recommended approach :-
    /**
     * 
     * @(#) UnitProxy. THIS CLASS CONTAIN OF CALCULATING THE GRADE BASED ON THE STUDENT 
     * ACHIVED MARKS IN ASSIGNMENTGRADE1, ASSIGNMENTGRADE2,QUIZ AND FINAL EXAM
     * Copyright (c) 1994-2016 Sun Microsystems, Inc.
     * 901 San Antonio Road, Palo Alto, California, 94303, U.S.A.
     * All rights reserved.
     *
     * This software is the confidential and proprietary information of Sun
     * Oracle, Inc. ("Confidential Information").  You shall not
     * disclose such Confidential Information and shall use it only in
     * accordance with the terms of the license agreement you entered into
     * with Sun.
     */
    package datamanagement;
    	public class UnitProxy implements IUnit {
        	private String UC;
        	private String un;
    2)Unclear of variable created by the developer, plus the naming pattern is not pratice as Java Standard. 
    
    Wrong practice / Not Recommended approach
    
    Line 24	private String UC;
    Line 25    	private String un;
    Line 28 	UnitManager um;
    Best practice / Recommened approach
    
    	private String unitCode;
        	private String unitName;
    	UnitManager unitManager;
    
    3). The methods in the UnitProxy.java, developer forgot to comment on each and every method is created. On top of that, method patten based on java standard is not practied in this class.
    
    Wrong Practice / Not recommended approach
    
    Line 46 public String getUnitCode() { return this.UC; }
    Line 58 public String getUnitName() { return this.un; }
    Line 70 public void setPsCutoff1(float cutoff) { um.getUnit(UC).setPsCutoff1(cutoff);}
    Line 82 public float getPsCutoff() { return um.getUnit(UC).getPsCutoff(); }
    Line 94 public void setCrCutoff(float cutoff) { um.getUnit(UC).setCrCutoff(cutoff); }
    Line 118 public void setDiCutoff(float cutoff) { um.getUnit(UC).setDiCutoff(cutoff); }
    Line 130 public float getDiCuttoff() { return um.getUnit(UC).getDiCuttoff(); }
    Line 142 public void setHdCutoff(float cutoff) { um.getUnit(UC).setHdCutoff(cutoff); }
    Line 166 public void setAeCutoff(float cutoff) { um.getUnit(UC).setAeCutoff(cutoff): }
    Line 178 public float getAeCutoff() { return um.getUnit(UC).getAeCutoff(); }
    Line 195 
    public String getGrade(float f1, float f2, float f3){ return um.getUnit(UC).getGrade(f1, f2, f3); }
    Line 202 
    public void addStudentRecord(IStudentUnitRecord record){ um.getUnit (UC) . AddStudentRecord record);}
    // All method in one line.
    Line 214 public IStudentUnitRecord getStudentRecord(int s){return um.getUnit (UC). getStudentRecord(s);}
    // All method in one line.
    Line 226 public StudentUnitRecordList listStudentRecords(){return um.getUnit (UC) . ListStudentRecords();}
    // All method in one line.
    Line 238 public int getAsg1Weight() {return um.getUnit(UC).getAsg1Weight(); }
    Line 250 public int getAsg2Weight() {return um.getUnit(UC).getAsg2Weight(); }
    Line 262 public int getExamWeight() {return um.getUnit(UC).getExamWeight(); }
    Best practice / Correct approach:-
    
    Line 262 public int getExamWeight() 
    		{
    			return um.getUnit(UC).getExamWeight(); 
    		}

--- ITC515_assignment2/src/datamanagement/UnitProxy.java
+++ ITC515_assignment2/src/datamanagement/UnitProxy.java
@@ -1,97 +1,276 @@
+/**
+ * @(#) UnitProxy. THIS CLASS CONTAIN OF CALCULATING THE GRADE BASED ON THE STUDENT
+ * ACHIVED MARKS IN ASSIGNMENTGRADE1, ASSIGNMENTGRADE2,QUIZ AND FINAL EXAM
+ * Copyright (c) 1994-2016 Sun Microsystems, Inc.
+ * 901 San Antonio Road, Palo Alto, California, 94303, U.S.A.
+ * All rights reserved.
+ *
+ *
+ * This software is the confidential and proprietary information of Sun
+ * Oracle, Inc. ("Confidential Information").  You shall not
+ * disclose such Confidential Information and shall use it only in
+ * accordance with the terms of the license agreement you entered into
+ * with Sun.
+ * Problem found in this code :-
+ *
+ * 1) Due to no comments on methods that is been implemented. Plus the variable that is created by the developer have
+ * failed to justify java standards in naming convensation.
+ * 2) This class act as a Interface of UnitProxy , if all the interface classes in one unitClass it would good.
+ *
+ */
 package datamanagement;
 
+/**
+ * First the public class variables, then the protected, then package level (no access modifier), and then the private.
+ * */
 public class UnitProxy implements IUnit {
-    private String UC;
-    private String un;
+    private String UC; // Unclear variable name plus should match a pattern . Example :- universityMarks / unitCode.
+    private String un; // Unclear variable name .
 
 
-    UnitManager um;
-
+    UnitManager um; // Thid does not explain what is um and where is UnitManagee is from. Need comment about the code.
+/**
+ * Following code do not contain basic comment what does it do.
+ * */
     public UnitProxy(String unitCode, String unitName) {
         this.UC = unitCode;
         this.un = unitName;
         um = UnitManager.UM();
     }
-
+    /**
+     * Following code do not contain basic comment what does it do.
+     * The method should follow proper pattern of creating a method.
+     * It should be
+     * public int methodName()
+     *  {
+     *      return x;
+     *  }
+     * */
     public String getUnitCode() {
         return this.UC;
     }
-
+    /**
+     * Following code do not contain basic comment what does it do.
+     * The method should follow proper pattern of creating a method.
+     * It should be
+     * public int methodName()
+     *  {
+     *      return x;
+     *  }
+     * */
     public String getUnitName() {
         return this.un;
     }
-
+    /**
+     * Following code do not contain basic comment what does it do.
+     * The method should follow proper pattern of creating a method.
+     * It should be
+     * public int methodName()
+     *  {
+     *      return x;
+     *  }
+     * */
     public void setPsCutoff1(float cutoff) {
         um.getUnit(UC).setPsCutoff1(cutoff);
     }
-
+    /**
+     * Following code do not contain basic comment what does it do.
+     * The method should follow proper pattern of creating a method.
+     * It should be
+     * public int methodName()
+     *  {
+     *      return x;
+     *  }
+     * */
     public float getPsCutoff() {
         return um.getUnit(UC).getPsCutoff();
     }
-
+    /**
+     * Following code do not contain basic comment what does it do.
+     * The method should follow proper pattern of creating a method.
+     * It should be
+     * public int methodName()
+     *  {
+     *      return x;
+     *  }
+     * */
     public void setCrCutoff(float cutoff) {
         um.getUnit(UC).setCrCutoff(cutoff);
     }
-
+    /**
+     * Following code do not contain basic comment what does it do.
+     * The method should follow proper pattern of creating a method.
+     * It should be
+     * public int methodName()
+     *  {
+     *      return x;
+     *  }
+     * */
     public float getCrCutoff() {
         return um.getUnit(UC).getCrCutoff();
     }
-
+    /**
+     * Following code do not contain basic comment what does it do.
+     * The method should follow proper pattern of creating a method.
+     * It should be
+     * public int methodName()
+     *  {
+     *      return x;
+     *  }
+     * */
     public void setDiCutoff(float cutoff) {
         um.getUnit(UC).setDiCutoff(cutoff);
     }
-
+    /**
+     * Following code do not contain basic comment what does it do.
+     * The method should follow proper pattern of creating a method.
+     * It should be
+     * public int methodName()
+     *  {
+     *      return x;
+     *  }
+     * */
     public float getDiCuttoff() {
         return um.getUnit(UC).getDiCuttoff();
     }
-
+    /**
+     * Following code do not contain basic comment what does it do.
+     * The method should follow proper pattern of creating a method.
+     * It should be
+     * public int methodName()
+     *  {
+     *      return x;
+     *  }
+     * */
     public void setHdCutoff(float cutoff) {
         um.getUnit(UC).setHdCutoff(cutoff);
     }
-
+    /**
+     * Following code do not contain basic comment what does it do.
+     * The method should follow proper pattern of creating a method.
+     * It should be
+     * public int methodName()
+     *  {
+     *      return x;
+     *  }
+     * */
     public float getHdCutoff() {
-
         return um.getUnit(UC).getHdCutoff();
     }
-
+    /**
+     * Following code do not contain basic comment what does it do.
+     * The method should follow proper pattern of creating a method.
+     * It should be
+     * public int methodName()
+     *  {
+     *      return x;
+     *  }
+     * */
     public void setAeCutoff(float cutoff) {
         um.getUnit(UC).setAeCutoff(cutoff);
     }
-
+    /**
+     * Following code do not contain basic comment what does it do.
+     * The method should follow proper pattern of creating a method.
+     * It should be
+     * public int methodName()
+     *  {
+     *      return x;
+     *  }
+     * */
     public float getAeCutoff() {
         return um.getUnit(UC).getAeCutoff();
     }
-
+    /**
+     * Following code do not contain basic comment what does it do.
+     * The method should follow proper pattern of creating a method.
+     * It should be
+     * public int methodName()
+     *  {
+     *      return x;
+     *  }
+     * */
     public String getGrade(float f1, float f2, float f3) {
         return um.getUnit(UC).getGrade(f1, f2, f3);
     }
-
+    /**
+     * Following code do not contain basic comment what does it do.
+     * The method should follow proper pattern of creating a method.
+     * It should be
+     * public int methodName()
+     *  {
+     *      return x;
+     *  }
+     * */
     public void addStudentRecord(IStudentUnitRecord record) {
         um.getUnit(UC).addStudentRecord(record);
     }
-
+    /**
+     * Following code do not contain basic comment what does it do.
+     * The method should follow proper pattern of creating a method.
+     * It should be
+     * public int methodName()
+     *  {
+     *      return x;
+     *  }
+     * */
     public IStudentUnitRecord getStudentRecord(int s) {
         return um.getUnit(UC).getStudentRecord(s);
     }
-
+    /**
+     * Following code do not contain basic comment what does it do.
+     * The method should follow proper pattern of creating a method.
+     * It should be
+     * public int methodName()
+     *  {
+     *      return x;
+     *  }
+     * */
     public StudentUnitRecordList listStudentRecords() {
         return um.getUnit(UC).listStudentRecords();
     }
-
+    /**
+     * Following code do not contain basic comment what does it do.
+     * The method should follow proper pattern of creating a method.
+     * It should be
+     * public int methodName()
+     *  {
+     *      return x;
+     *  }
+     * */
     public int getAsg1Weight() {
         return um.getUnit(UC).getAsg1Weight();
     }
-
+    /**
+     * Following code do not contain basic comment what does it do.
+     * The method should follow proper pattern of creating a method.
+     * It should be
+     * public int methodName()
+     *  {
+     *      return x;
+     *  }
+     * */
     public int getAsg2Weight() {
         return um.getUnit(UC).getAsg2Weight();
     }
-
+    /**
+     * Following code do not contain basic comment what does it do.
+     * The method should follow proper pattern of creating a method.
+     * It should be
+     *  public int getExamWeight()
+     *  {
+     *      return um.getUnit(UC).getExamWeight();
+     *  }
+     * */
     public int getExamWeight() {
         return um.getUnit(UC).getExamWeight();
     }
-
+    /**
+     * Following code do not contain basic comment what does it do.
+     * The variable needs to be clearly define. int asg1Wgt, int asg2Wgt, int examWgt.
+     * More understandable manner example :- int assignment1Weight
+     * */
     public void setAssessmentWeights(int asg1Wgt, int asg2Wgt, int examWgt) {
         um.getUnit(UC).setAssessmentWeights(asg1Wgt, asg2Wgt, examWgt);
-
     }
 }
