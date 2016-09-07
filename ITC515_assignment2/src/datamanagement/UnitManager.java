421456490636372bc3d5f4643fb5e46dbe1d796a
Tom <hzl410@gmail.com>
2016-09-07 21:06:19 +1000

    add comments

--- ITC515_assignment2/src/datamanagement/UnitManager.java
+++ ITC515_assignment2/src/datamanagement/UnitManager.java
@@ -24,14 +24,31 @@ public class UnitManager {
 	}
 
 	private IUnit createUnit(String unitCode) {
+        /* The variable name should be meaningful */
 		IUnit iu;
 
+        /* suggest init a list variable before use. */
 		for (Element el : (List<Element>) XMLManager.getXML().getDocument()
 				.getRootElement().getChild("unitTable").getChildren("unit"))
 			if (unitCode.equals(el.getAttributeValue("uid"))) {
 				StudentUnitRecordList slist;
                 /* slist is never used*/
 				slist = null;
+                /* unboxing can be safely removed
+                * Suggestion:
+                * iu = new Unit(el.getAttributeValue("uid"),
+				*		el.getAttributeValue("name"), Float.valueOf(
+                *        el.getAttributeValue("ps")), Float
+                *        .valueOf(el.getAttributeValue("cr")), Float.valueOf(
+                *        el.getAttributeValue("di")), Float
+                *        .valueOf(el.getAttributeValue("hd")), Float.valueOf(
+                *        el.getAttributeValue("ae")),
+                *        Integer.valueOf(el.getAttributeValue("asg1wgt")), Integer.valueOf(
+                *        el.getAttributeValue("asg2wgt")),
+                *        Integer.valueOf(el.getAttributeValue("examwgt")), StudentUnitRecordManager
+				*				.instance().getRecordsByUnit(unitCode));
+				* UM.put(iu.getUnitCode(), iu);
+				*/
 				iu = new Unit(el.getAttributeValue("uid"),
 						el.getAttributeValue("name"), Float.valueOf(
 								el.getAttributeValue("ps")).floatValue(), Float
@@ -59,6 +76,7 @@ public class UnitManager {
 		IUnit iu;
 
 		uM = new UnitMap();
+        /* suggest init a list variable before use. */
 		for (Element el : (List<Element>) XMLManager.getXML().getDocument()
 				.getRootElement().getChild("unitTable").getChildren("unit")) {
 			iu = new UnitProxy(el.getAttributeValue("uid"),
