759ffa4317058dcba8d9e6b918d59e7c3a5c5d5e
saidulhoque <saidul.office@gmail.com>
2016-09-08 13:03:25 +1000

    Update IStudentLister.java

--- ITC515_assignment2/src/datamanagement/IStudentLister.java
+++ ITC515_assignment2/src/datamanagement/IStudentLister.java
@@ -7,5 +7,8 @@ package datamanagement;
 public interface IStudentLister {
 
     public void clearStudents();
-    public void addStudent(IStudent student);//There should be single blank line between methods
+    public void addStudent(IStudent student);
+    //There should be single blank line  between methods
+    //The modifier public is redundant
+    //public can be removed
 }
