b96ae8681c2a453c9e8ef94de287181dbba2f5c5
saidulhoque <saidul.office@gmail.com>
2016-09-08 22:47:15 +1000

    Update ListUnitsCTL.java

--- ITC515_assignment2/src/datamanagement/ListUnitsCTL.java
+++ ITC515_assignment2/src/datamanagement/ListUnitsCTL.java
@@ -1,8 +1,17 @@
 package datamanagement;
+
 public class ListUnitsCTL {
     private UnitManager um;
-public ListUnitsCTL() { um = UnitManager.UM(); } //Error: opening and closing braces should not be in one line  and indentation. method is not validated
-            public void listUnits( IUnitLister lister ) {
-lister.clearUnits();UnitMap units = um.getUnits(); //indentation
-        for (String s : units.keySet() ) //semicolon missing in empty for statement and indentation
-            lister.addUnit(units.get(s));}} //closing braces should be on different line and indentation
+
+    public ListUnitsCTL() {
+        um = UnitManager.UM();
+    }
+
+    public void listUnits(IUnitLister lister) {
+        lister.clearUnits();
+        UnitMap units = um.getUnits();
+        //In for loop the statement must be within braces.
+        for (String s : units.keySet())
+            lister.addUnit(units.get(s));
+    }
+}
